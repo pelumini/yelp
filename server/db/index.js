377a958399deccd0ae360b77944896b04edea363
pelumini <pelumini@gmail.com>
2020-08-20 14:11:42 +0100

    change db to prod

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "yelp",
-  password: "Emma@nuel1",
-  port: 5432,
-});
-
 // const pool = new Pool({
 //   user: "postgres",
-//   host: "yelp.cgegxl9wteno.us-east-1.rds.amazonaws.com",
+//   host: "localhost",
 //   database: "yelp",
-//   password: "Emma1Nuel",
+//   password: "Emma@nuel1",
 //   port: 5432,
 // });
+
+const pool = new Pool({
+  user: "postgres",
+  host: "yelp.cgegxl9wteno.us-east-1.rds.amazonaws.com",
+  database: "yelp",
+  password: "Emma1Nuel",
+  port: 5432,
+});
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
